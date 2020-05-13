d6315f07e14873949d51f2d6ff738a1eb1b2a6d1
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2020-05-13 15:51:53 -0700

    gpu: impl_list: remove repeated primitive

--- src/gpu/gpu_impl_list.cpp
+++ src/gpu/gpu_impl_list.cpp
@@ -70,7 +70,6 @@ static const pd_create_f gpu_impl_list[] = {
         // Convolution
         INSTANCE(ocl::gen12hp_1x1_convolution_fwd_t),
         INSTANCE(ocl::gen12hp_bf16_convolution_bwd_weights_t),
-        INSTANCE(ocl::gen12hp_1x1_convolution_fwd_t),
         INSTANCE(ocl::gen12hp_convolution_fwd_t),
         INSTANCE(ocl::gen12hp_convolution_bwd_data_t),
         INSTANCE(ocl::gen12lp_x8s8s32x_1x1_convolution_fwd_t),
